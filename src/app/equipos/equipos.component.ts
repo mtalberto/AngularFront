6702a1f4830f7923ffb74a9446015a527c52d9da
albertomt <alberto.fachan@gmail.com>
2024-03-04 20:53:56 +0100

    Pagina en angular

---
+++ src/app/equipos/equipos.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipos',
+  standalone: true,
+  imports: [],
+  templateUrl: './equipos.component.html',
+  styleUrl: './equipos.component.scss'
+})
+//styleUrls es para añadir mas de un estilo [ estilos]
+export class EquiposComponent {
+
+}
